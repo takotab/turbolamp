57bc880451710aa61abb4e8b6b969e46818b9a5d
tako_desktop <takotabak@gmail.com>
2020-05-01 16:36:48 +0200

    extra sidebar selectbox

--- app/app.py
+++ app/app.py
@@ -4,7 +4,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-
 def main():
     data = pd.read_csv(
         "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv",
@@ -26,6 +25,13 @@ def main():
     for land in current_lands:
         data[data.country == land].value.plot(ax=ax, label=land, logy=True)
     # data[data.country == "Algeria"].value.plot(ax=ax, label="Algeria")
+
+    for i in range(4):
+        new_land = st.sidebar.selectbox(
+            f"Which other country {i}?", sorted(set(data.country))
+        )
+        data[data.country == new_land].value.plot(ax=ax, label=new_land, logy=True)
+
     f.legend()
     st.pyplot(f)
 
